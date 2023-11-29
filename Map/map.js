8f8f3764ddd8c5ddc3c104d9f095d1f3bcbe3154
Ascendev <AscendCode@outlook.fr>
2023-11-29 13:25:02 +0100

    Base main + map

---
+++ Map/map.js
@@ -0,0 +1,32 @@
+import * as THREE from '../three.module.js'
+
+export class Map {
+    planeGeo = new THREE.PlaneGeometry(0.90, 0.90, 10, 10);
+    planeWhiteMat = new THREE.MeshStandardMaterial ({color:"white"});
+    planeRedMat = new THREE.MeshStandardMaterial ({color:"grey"});
+    planeList = [];
+    spawn = [0,0];
+    constructor(scene){
+        this.scene = scene;
+        this.plane = null;
+        for (let i = 0; i < 10; i++){
+            for (let j = 0; j < 10; j++){
+                if ((i == 8 && j < 8) || (i < 8 && i > 3 && j == 7)||
+                    (i == 4 && j < 8 && j >1) || (j==2 && i > 0 && i < 4)||
+                    (i == 1 && j > 1))
+                    this.plane = new THREE.Mesh(this.planeGeo, this.planeRedMat);
+                else this.plane = new THREE.Mesh(this.planeGeo, this.planeWhiteMat);
+                this.plane.position.x = -4.5 + j;
+                this.plane.position.z = 2.5 - i;
+                this.plane.rotation.x = THREE.MathUtils.degToRad(-90);
+                this.plane.receiveShadow = true;
+                this.scene.add(this.plane);
+                this.planeList.push(this.plane);
+            }
+        }
+        this.spawn = this.planeList[80];
+    }
+    get spawn(){
+        return (this.spawn);
+    }
+}
--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import * as THREE from './three.module.js'
 import { OrbitControls } from './OrbitControls.js';
-import { purpleEnemy } from "./Enemy/Enemy.js";
-
 //canva setup
 const cnv = document.getElementById("screen");
 //renderer et camera
@@ -14,7 +12,6 @@ camera.position.y = 5;
 camera.rotation.x = THREE.MathUtils.degToRad(-45);
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
-
 //lumières
 const directionalLight = new THREE.DirectionalLight(0xffffff, 4.0);
 directionalLight.position.y = 4;
@@ -25,29 +22,12 @@ scene.add(directionalLight);
 scene.add(helper);
 
 //map temporaire
-const planeGeo = new THREE.PlaneGeometry(0.90, 0.90, 10, 10);
-const planeWhiteMat = new THREE.MeshStandardMaterial ({color:"white"});
-const planeRedMat = new THREE.MeshStandardMaterial ({color:"grey"});
-const planeList = [];
-let plane;
-for (let i = 0; i < 10; i++){
-    for (let j = 0; j < 10; j++){
-        if ((i == 8 && j < 8) || (i < 8 && i > 3 && j == 7)||
-            (i == 4 && j < 8 && j >1) || (j==2 && i > 0 && i < 4)||
-            (i == 1 && j > 1))
-            plane = new THREE.Mesh(planeGeo, planeRedMat);
-        else plane = new THREE.Mesh(planeGeo, planeWhiteMat);
-        plane.position.x = -4.5 + j;
-        plane.position.z = 2.5 - i;
-        plane.rotation.x = THREE.MathUtils.degToRad(-90);
-        plane.receiveShadow = true;
-        scene.add(plane);
-        planeList.push(plane);
-    }
-}
+import {Map} from "./Map/map.js"
+const map = new Map(scene);
+const spawn = [map.spawn.position.x-0.15, map.spawn.position.z + 0.15];
 
 //Enemies
-const spawn = [planeList[80].position.x-0.15, planeList[80].position.z + 0.15];
+import { purpleEnemy } from "./Enemy/Enemy.js";
 const enemies = [];
 let enemy = new purpleEnemy(spawn[0], spawn[1], scene);
 enemies.push(enemy);
